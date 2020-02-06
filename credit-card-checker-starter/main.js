2b6d499792768b4fc14055356a3a0d61a4d03b01
Veronica HW <veronica.wajda@gmail.com>
2020-02-06 11:28:57 -0700

    Luhn algorithm for checking validity of credit cards

---
+++ credit-card-checker-starter/main.js
@@ -0,0 +1,102 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+//    index       0. 1. 2. 3. 4. 5. 6. 7. 8. 9 10 11 12 13 14 15
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+function isDivByTen(num){
+  return (num % 10 === 0 );
+}
+
+function validateCred(array){
+  let sum = 0;
+  // Fail fast
+  if (array.length > 16 || array.length < 15){
+    console.log (`${array.join('')} - Credit card is invalid FF`);
+    return 'invalid';
+  } else if(array.length === 15){
+    array.unshift(0);
+  }
+  array.forEach(function(item, index){
+    if(index % 2 === 0){
+      item = item * 2;
+      if(item > 9){
+        item = item - 9;
+      }
+    }
+    sum += item;
+    });
+    if (isDivByTen(sum)){
+      console.log(`${array.join('')} - Credit card is valid`);
+      return 'valid';
+    } else {
+      console.log(`${array.join('')} - Credit card is invalid`);
+      return 'invalid';
+    }
+}
+
+function findInvalidCards(batch){
+  const validCards = [];
+  const invalidCards = [];
+  batch.forEach(function(cardNum) {
+    if(validateCred(cardNum) === 'valid'){
+      validCards.push(cardNum);
+    } else {
+      invalidCards.push(cardNum);
+    }
+  });
+  return invalidCards;
+}
+
+
+function idInvalidCardCompanies(batch){
+  let allCards = [];
+  let invalidCompanies = [];
+  allInvalidCompanies = findInvalidCards(batch);
+
+  allInvalidCompanies.forEach(function(cardNum){
+    if(cardNum[0] === 0){
+      cardNum = cardNum.slice(1);
+    }
+    if (cardNum[0] === 3){
+      if(invalidCompanies.indexOf('Amex') === -1){
+        invalidCompanies.push('Amex');
+      }
+    } else if(cardNum[0] === 4){
+      if(invalidCompanies.indexOf('Visa') === -1){
+        invalidCompanies.push('Visa');
+      }
+    } else if(cardNum[0] === 5){
+      if(invalidCompanies.indexOf('Mastercard') === -1){
+        invalidCompanies.push('Mastercard');
+      }
+    } else if(cardNum[0] === 6){
+      if(invalidCompanies.indexOf('Discover') === -1){
+        invalidCompanies.push('Discover');
+      }
+    } else {
+      console.log(`Company not found for card ${cardNum.join(' ')}`);
+    }
+  });
+  console.log(`Invalid card companies are: ${invalidCompanies.join(' ')}`);
+}
+
+idInvalidCardCompanies(batch);
